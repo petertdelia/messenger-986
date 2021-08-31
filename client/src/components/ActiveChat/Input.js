2ff1637edf30c6e49a4f0a454d9717768854dafa
Peter D'Elia <petertdelia@gmail.com>
2021-08-31 11:26:16 -0400

    added read status to new messages

--- client/src/components/ActiveChat/Input.js
+++ client/src/components/ActiveChat/Input.js
@@ -33,7 +33,8 @@ const Input = (props) => {
       text: event.target.text.value,
       recipientId: otherUser.id,
       conversationId,
-      sender: conversationId ? null : user
+      sender: conversationId ? null : user,
+      read: otherUser.activeChat === user.username
     };
     await postMessage(reqBody);
     setText("");
