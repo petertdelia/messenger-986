011118df83535b4d899de2b5e8d76ec957c53589
Peter D'Elia <petertdelia@gmail.com>
2021-09-21 15:04:27 -0400

    fixed button

--- client/src/LoginSignupButtons.js
+++ client/src/LoginSignupButtons.js
@@ -7,6 +7,7 @@ import {
   Button,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 const useStyles = makeStyles((theme) => ({
   form: {
@@ -20,19 +21,26 @@ const useStyles = makeStyles((theme) => ({
     height: "9vh",
     borderRadius: "5px",
   },
+  mobileButton: {
+    boxShadow: "1px -1px 5px 2px #C9C9C9",
+    margin: "5% 5%",
+    width: "30vw",
+    height: "9vh",
+    borderRadius: "5px",
+  }
 }));
 
 const LoginSignupButtons = (props) => {
   const history = useHistory();
   const classes = useStyles();
   const { leftText, buttonText, type } = props;
-
+  const isWideScreen = useMediaQuery('(min-width:600px)');
 
 
   return (
     <Grid container item style={{justifyContent: "flex-end", alignItems: "center", flex: 1}}>
       <Typography color="secondary" variant="subtitle1">{leftText}</Typography>
-      <Button className={classes.signupButton} color="primary" onClick={() => history.push(`/${type}`)}>
+      <Button className={isWideScreen ? classes.signupButton : classes.mobileButton} color="primary" onClick={() => history.push(`/${type}`)}>
         <Typography variant="subtitle1">{buttonText}</Typography>
       </Button>
     </Grid>
